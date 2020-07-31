a7fc1c9e662af12757b83830457a682521bfd973
Sam Huynh <samhwang2112.dev@gmail.com>
2020-07-31 16:08:09 +1000

    Fix typo in loading composer module

--- general/load/composer.sh
+++ general/load/composer.sh
@@ -1,4 +1,4 @@
-if ! type "composer" &> /dev/null; then
+if type composer > /dev/null 2>&1; then
   [ -d $HOME/.composer/vendor/bin ] && export PATH=$PATH:$HOME/.composer/vendor/bin
   [ -d $HOME/.config/composer/vendor/bin ] && export PATH=$PATH:$HOME/.config/composer/vendor/bin
 fi
