dddc4362663cac8ee7e1ef3ee829bb81e97e45e3
Sam Huynh <samhwang2112.dev@gmail.com>
2021-02-15 09:36:31 +1100

    Add a function to update all git dotfile submodules

--- general/load/package.sh
+++ general/load/package.sh
@@ -1,5 +1,11 @@
 if [ $CURRENT_OS = "Darwin" ]; then
-    alias pkg_up="brew update; brew upgrade; brew cleanup;"
+    alias update_packages="brew update; brew upgrade; brew cleanup;"
 elif [ $CURRENT_OS = "Linux" ]; then
-    alias pkg_up="sudo apt update; sudo apt upgrade -y; sudo apt autoremove -y;"
+    alias update_packages="sudo apt update; sudo apt upgrade -y; sudo apt autoremove -y;"
 fi
+
+function pkg_up() {
+    set -x;
+    update_packages;
+    git --git-dir $DOTFILEPATH/.git submodule update --recursive --remote;
+}
