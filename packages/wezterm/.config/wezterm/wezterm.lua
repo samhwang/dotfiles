e57f013db2472853dbb13bf3bbb704f824603562
Sam Huynh <samhwang2112.dev@gmail.com>
2024-10-10 16:14:55 +1100

    feat: add tab title format to wezterm

--- packages/wezterm/.config/wezterm/wezterm.lua
+++ packages/wezterm/.config/wezterm/wezterm.lua
@@ -1,3 +1,4 @@
+local wezterm = require 'wezterm'
 local keybinds = require 'keybinds'
 local scrollback = require 'scrollback'
 local theme = require 'theme'
@@ -7,5 +8,39 @@ local config = {}
 keybinds.apply_to_config(config)
 scrollback.apply_to_config(config)
 theme.apply_to_config(config)
+
+local function get_current_working_dir(tab)
+  local current_dir = tab.active_pane and tab.active_pane.current_working_dir or { file_path = '' }
+  local HOME_DIR = os.getenv('HOME')
+
+  return current_dir.file_path == HOME_DIR and '~'
+    or string.gsub(current_dir.file_path, '(.*[/\\])(.*)', '%2')
+end
+
+local function get_process(tab)
+  if not tab.active_pane or tab.active_pane.foreground_process_name == '' then
+    return nil
+  end
+
+  local process_name = string.gsub(tab.active_pane.foreground_process_name, '(.*[/\\])(.*)', '%2')
+
+  return string.format('%s', process_name)
+end
+
+wezterm.on(
+    'format-tab-title',
+    function(tab, tabs, panes, config, hover, max_width)
+        local cwd = wezterm.format({
+            { Text = get_current_working_dir(tab) },
+        })
+
+        local process = get_process(tab)
+        local title = process and string.format(' %s (%s) ', process, cwd) or ' [?] '
+
+        return {
+            { Text = title },
+        }
+    end
+)
 
 return config
