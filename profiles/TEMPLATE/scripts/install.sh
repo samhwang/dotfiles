e273257128a8113cb1ecf0def113a59d42075283
Sam Huynh <samhwang2112.dev@gmail.com>
2025-08-07 10:39:20 +1000

    profiles: update the template command

--- profiles/TEMPLATE/scripts/install.sh
+++ profiles/TEMPLATE/scripts/install.sh
@@ -8,8 +8,8 @@ stow --target=$HOME *
 cd ..
 
 cd ~/.dotfiles/profiles
-OP_ACCOUNT=my.1password.com op inject -i linux/.config/profiles/private.zshrc.tpl -o linux/.config/profiles/private.zshrc
-stow --target=$HOME linux
+OP_ACCOUNT=my.1password.com op inject -i TEMPLATE/.config/profiles/private.zshrc.tpl -o TEMPLATE/.config/profiles/private.zshrc
+stow --target=$HOME TEMPLATE
 cd ..
 
 # Install the rest
