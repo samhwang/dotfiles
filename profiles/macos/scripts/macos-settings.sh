dd1bc249e284b16ea093a21bb7e3f816f12f548d
Sam Huynh <samhwang2112.dev@gmail.com>
2025-08-20 12:02:25 +1000

    macos: update key repeat settings
    Signed-off-by: Sam Huynh <samhwang2112.dev@gmail.com>

--- profiles/macos/scripts/macos-settings.sh
+++ profiles/macos/scripts/macos-settings.sh
@@ -66,8 +66,8 @@ defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 ##########
 # Screen #
