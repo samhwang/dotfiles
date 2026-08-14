ae9eb10f783032c957966423bd326dc0773f397d
Sam Huynh <samhwang2112.dev@gmail.com>
2026-08-14 19:02:42 +1000

    cachyos: add function to swap DM

--- profiles/linux-cachyos/.config/profiles/profile.zsh
+++ profiles/linux-cachyos/.config/profiles/profile.zsh
@@ -11,3 +11,25 @@ alias zed='zeditor'
 export XMODIFIERS="@im=fcitx"
 
 alias sso-vait="aws ssm start-session --target i-09bfcbbea7ac38621 --profile vait"
+
+function switch_greetd() {
+    echo "This function requires sudo. Prompting sudo rights now."
+    sudo -v
+
+    echo "Switching to greetd..."
+    sudo systemctl disable sddm.service --quiet
+    sudo systemctl enable greetd.service --quiet
+
+    echo "Switch complete. Reboot for changes to take effect."
+}
+
+function switch_sddm() {
+    echo "This function requires sudo. Prompting sudo rights now."
+    sudo -v
+
+    echo "Switching to sddm..."
+    sudo systemctl disable greetd.service --quiet
+    sudo systemctl enable sddm.service --quiet
+
+    echo "Switch complete. Reboot for changes to take effect."
+}
