bf63e02e5f45a93204ba4f49c1d934b021f7f784
Sam Huynh <samhwang2112.dev@gmail.com>
2026-08-17 21:38:21 +1000

    zsh: uncomment zsh welcome auto

--- packages/zsh/.config/zsh/zellij.zsh
+++ packages/zsh/.config/zsh/zellij.zsh
@@ -2,11 +2,5 @@
 if [[ -o interactive ]]; then
   # Set Zellij config dir so we can launch it manually too
   export ZELLIJ_CONFIG_DIR="$HOME/.config/zellij"
-
-  # Only auto-launch Zellij inside Ghostty
-  if [[ "$TERM" = "xterm-ghostty" ]]; then
-    if [[ -z "$ZELLIJ" ]]; then
-      zellij -l welcome
-    fi
-  fi
+  alias zellij-welcome='zellij -l welcome'
 fi
